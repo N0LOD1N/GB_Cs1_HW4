f2637c1fad03e75b5120c583f703212829ce4da4
Nik <hemper@mail.ru>
2023-06-23 17:52:48 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("Задача 25--------------------");
+void Cubify(int numA, int numB)
+{
+    int result = numA;
+    for (int i = 1; i < numB; i++)
+    {
+        result *= numA;
+    }
+    Console.WriteLine($" {numA}^{numB} = {result}");
+}
+
+Console.WriteLine("Введите два числа:");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Cubify(numA, numB);
+
+Console.WriteLine("\nЗадача 27--------------------");
+
+void Summify(int num)
+{
+    int sum = 0;
+    while (num != 0) 
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    Console.WriteLine($"Сумма всех цифр заданного числа равна {sum}.");
+}
+
+Console.Write("Введите число и узнайте сумму его цифр: ");
+numA = Convert.ToInt32(Console.ReadLine());
+Summify(numA);
+
+Console.WriteLine("\nЗадача 29--------------------");
+
+void RandomArray()
+{
+    int[] array = new int[new Random().Next(1, 100)];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1) Console.Write(", ");
+        else Console.WriteLine(".");
+    }
+}
+
+Console.Write("Нажмите Enter, чтобы сгенерировать массив.");
+Console.ReadLine();
+RandomArray();
